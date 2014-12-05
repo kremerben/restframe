f26b12bf444e7db9ae631a2b66e8b114a8a2e514
kremerdesign <kremerdesign@gmail.com>
2014-12-05 15:04:01 -0800

    requests-and-responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = patterns('',
@@ -11,3 +12,6 @@ urlpatterns = patterns('',
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
